a96ead3e0b8d7d3f955251700f3b603c046297f1
SehajDhanjal <sehajdhanjal09@gmail.com>
2026-06-19 00:44:10 +0530

    Create App component with loading message

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,3 @@
-
+export default function App() {
+  return <div style={{color:"white",background:"black",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontFamily:"sans-serif",fontSize:24}}>STRYD Loading...</div>
+}
