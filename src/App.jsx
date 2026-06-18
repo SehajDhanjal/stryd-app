9b034a5c0d3919d2692631423cae5b628249bff2
SehajDhanjal <sehajdhanjal09@gmail.com>
2026-06-19 01:14:46 +0530

    Remove App function and import React

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,1 @@
-export default function App() {
-  return <div style={{color:"white",background:"black",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontFamily:"sans-serif",fontSize:24}}>STRYD Loading...</div>
-}
+import React from 'react'
