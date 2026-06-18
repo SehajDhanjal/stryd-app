52cc32d148c0534092500d38fba341ab658053e3
SehajDhanjal <sehajdhanjal09@gmail.com>
2026-06-19 01:04:57 +0530

    Refactor App component to use React.createElement

--- main.jsx
+++ main.jsx
@@ -1,3 +1,7 @@
-export default function App() {
-  return <div style={{color:"white",background:"black",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontFamily:"sans-serif",fontSize:24}}>STRYD Loading...</div>
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './src/App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(App)
+)
